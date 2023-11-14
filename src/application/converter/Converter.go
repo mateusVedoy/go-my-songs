93eed73d89c3f8759af2951bcbc454dfd5d5e769
mateusVedoy <mateusvedoy@outlook.com>
2023-11-14 16:56:29 -0300

    implements Converter

---
+++ src/application/converter/Converter.go
@@ -0,0 +1,26 @@
+package converter
+
+import (
+	"github.com/mateusVedoy/go-my-songs.git/src/application/dto"
+	"github.com/mateusVedoy/go-my-songs.git/src/domain"
+)
+
+type Converter struct{}
+
+func (c Converter) ToDomain(dto dto.MusicDTO) (*domain.Music, *domain.BusinessError) {
+	music, err := domain.NewMusic(dto.Name, dto.Album, dto.Artist, dto.Duration)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return music, nil
+}
+
+func (c Converter) ToDTO(entity domain.Music) *dto.MusicDTO {
+	return nil
+}
+
+func NewConverterImpl() *Converter {
+	return &Converter{}
+}
