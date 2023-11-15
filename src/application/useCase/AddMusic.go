e58317730969e80175ec3bb8c67905586204b779
mateusVedoy <mateusvedoy@outlook.com>
2023-11-15 00:24:26 -0300

    add in memory db and link it to use cases

--- src/application/useCase/AddMusic.go
+++ src/application/useCase/AddMusic.go
@@ -5,20 +5,23 @@ import (
 
 	"github.com/mateusVedoy/go-my-songs.git/src/application/api"
 	"github.com/mateusVedoy/go-my-songs.git/src/application/dto"
+	"github.com/mateusVedoy/go-my-songs.git/src/infrastructure/database"
 )
 
-func (manager Manager) AddMusic(dto dto.MusicDTO) (*api.ResponseSuccess, *api.ResponseError) {
-	music, err := manager.converter.ToDomain(dto)
+func (manager Manager) AddMusic(musicDTO dto.MusicDTO) (*api.ResponseSuccess, *api.ResponseError) {
+	music, err := manager.converter.ToDomain(musicDTO)
 
 	if err != nil {
 		responseErr := api.SetErrorResponse(400, "There are errors in your request. Check it bellow", err.GetErrors())
 		return nil, responseErr
 	}
 
-	fmt.Println(music)
+	database.DB = append(database.DB, *music)
+	fmt.Println("Music saved!")
 
-	manager.Music = append(manager.Music, *music)
+	var dtoArr []dto.MusicDTO
+	dtoArr = append(dtoArr, musicDTO)
 
-	responseSuccess := api.SetSuccessResponse(201, "Music added successfully!", manager.Music)
+	responseSuccess := api.SetSuccessResponse(201, "Music added successfully!", dtoArr)
 	return responseSuccess, nil
 }
---
+++ src/infrastructure/database/InMemoryDB.go
@@ -0,0 +1,5 @@
+package database
+
+import "github.com/mateusVedoy/go-my-songs.git/src/domain"
+
+var DB = []domain.Music{}
